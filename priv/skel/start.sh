6a9fe3af4854444ca425824ee3c0f33d95dff11a
Abhay Kumar <abhay+git@opensynapse.net>
2008-05-30 19:12:23 -0700

    some startup options and named node

--- priv/skel/start.sh
+++ priv/skel/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -config conf/skel -s skel
+exec erl +Bc +K true -smp auto -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name skel -config conf/skel -s skel
